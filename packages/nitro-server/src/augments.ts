ccb53e0ebf8b4dbe8d5607d6543fdcc73da8804c
Daniel Roe <daniel@roe.dev>
2026-03-23 15:55:38 +0000

    docs: add missing jsdoc strings

--- packages/nitro-server/src/augments.ts
+++ packages/nitro-server/src/augments.ts
@@ -78,6 +78,8 @@ declare module '@nuxt/schema' {
     /**
      * Called before Nitro writes `.nuxt/tsconfig.server.json`, allowing addition of custom references and declarations.
      * @param options Objects containing `references`, `declarations`
+     * @param options.references Array of TypeScript references to add
+     * @param options.declarations Array of declaration strings to add
      * @returns Promise
      */
     'nitro:prepare:types': (options: { references: TSReference[], declarations: string[] }) => HookResult
@@ -183,6 +185,8 @@ declare module 'nuxt/schema' {
     /**
      * Called before Nitro writes `.nuxt/tsconfig.server.json`, allowing addition of custom references and declarations.
      * @param options Objects containing `references`, `declarations`
+     * @param options.references Array of TypeScript references to add
+     * @param options.declarations Array of declaration strings to add
      * @returns Promise
      */
     'nitro:prepare:types': (options: { references: TSReference[], declarations: string[] }) => HookResult
